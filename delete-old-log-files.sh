34a8b9aa63dffe3a9ca889e8cef3d2339a8b01c1
sowjanya <devopssowjanya88s@gmail.com>
2026-03-03 16:26:48 +0530

    new

--- delete-old-log-files.sh
+++ delete-old-log-files.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 id=$(id -u)
-source_dir="/app-log"
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/$(basename "$0").log"
 
 if [ $id -ne 0 ]; then
 echo "please run with root user credentials"
 exit 1
 fi
 
-if [ ! -d $source_dir ] ; then
+if [ ! -d $LOGS_DIR ] ; then
 echo "$source_dir does not exists"
-else
-Files=$(find $source_dir -name "*.log" -type f -mtime +14)
+fi
+
+
+Files=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
+if [ -z $Files ] ; then
+   echo " files older than 14 days does not exists"
+   else
     while read -r line; do
         echo "files to be deleted: $line"
         rm -f $line
         echo "$line deleted"
     done <<< "$Files"
  fi
+ 
 
