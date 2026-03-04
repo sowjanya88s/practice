e21ba4609c3060684ad8d82c0902e165ed7eed88
sowjanya <devopssowjanya88s@gmail.com>
2026-03-04 21:05:08 +0530

    new

--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@ alert_type=$3
 ip_address=$4
 to_team=$5
 
-final_message_body=$(echo "$FINAL_MESSAGE" | sed -e 's/[]\/$*.^[]/\\&/g')
+final_message_body=$(echo "$message_body" | sed -e 's/[]\/$*.^[]/\\&/g')
 message_body=$(sed -e "s/to_team/$TO_TEAM/g"   -e "s/message/$final_message_body/g" -e "s/alert_type/$alert_type/g" -e "s/ip_address/$ip_address" template.html)
 
 {
